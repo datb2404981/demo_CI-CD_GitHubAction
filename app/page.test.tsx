274851bbd7423965890c6d2cb7c1740dbb387e50
Văn_Đạt <dat7092006@gmail.com>
2026-06-26 15:14:20 +0700

    Test luồng đẩy code lên Production

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -4,12 +4,6 @@
 import { render, screen } from "@testing-library/react";
 import Page from "./page";
 
-it("App Router: Works with Server Components", () => {
-  render(<Page />);
-  expect(screen.getByRole("heading")).toHaveTextContent("App Router");
-});
-
-
 it("should fail this test intentionally", () => {
   expect("hello world").toBe("hello world");
 });
